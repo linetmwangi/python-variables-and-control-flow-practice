5c9c3eaf55f2e241ec7403b7b125ee6f57bbe304
linetmwangi <mwangilinet45@gmail.com>
2024-10-23 17:18:30 +0300

    added python files

---
+++ question1.py
@@ -0,0 +1,19 @@
+#1.Variable Assignment & Arithmetic Operations
+# Assign values to variables a and b
+a = 10
+b = 5
+
+# Perform operations and print the results
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+# Output the results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+
+
